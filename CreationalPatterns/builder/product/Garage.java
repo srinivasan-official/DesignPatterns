12138bda33e44f57116837ac6e0466597d33b6ac
Srinivasan <srinivasan12798@gmail.com>
2022-03-15 07:07:14 +0530

    Builder Impl

---
+++ CreationalPatterns/builder/product/Garage.java
@@ -0,0 +1,17 @@
+package builder.product;
+
+public class Garage {
+	private String garageType;
+
+	public Garage(String garageType) {
+		this.garageType = garageType;
+	}
+	
+	public String getGarageType() {
+		return garageType;
+	}
+
+	public void setGarageType(String garageType) {
+		this.garageType = garageType;
+	}
+}
---
+++ CreationalPatterns/builder/product/Garden.java
@@ -0,0 +1,17 @@
+package builder.product;
+
+public class Garden {
+	private int sqFeet;
+	
+	public Garden(int sqFeet) {
+		this.sqFeet = sqFeet;
+	}
+	
+	public int getSqFeet() {
+		return sqFeet;
+	}
+	
+	public void setSqFeet(int sqFeet) {
+		this.sqFeet = sqFeet;
+	}
+}
